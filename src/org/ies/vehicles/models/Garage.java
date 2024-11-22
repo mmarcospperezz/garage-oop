38b84f21f28af286b7232589defeed32c4db524a
mmarcospperezz <mmarcospperez2204@gmail.com>
2024-11-22 12:53:41 +0100

    Implementado Vehicule

--- src/org/ies/vehicles/models/Garage.java
+++ src/org/ies/vehicles/models/Garage.java
@@ -1,4 +1,52 @@
 package org.ies.vehicles.models;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Garage {
+    private String name;
+    private String direction;
+    private String[] vehicule;
+
+    public Garage(String name, String direction, String[] vehicule) {
+        this.name = name;
+        this.direction = direction;
+        this.vehicule = vehicule;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String[] getVehicule() {
+        return vehicule;
+    }
+
+    public void setVehicule(String[] vehicule) {
+        this.vehicule = vehicule;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Garage garage = (Garage) o;
+        return Objects.equals(name, garage.name) && Objects.equals(direction, garage.direction) && Objects.deepEquals(vehicule, garage.vehicule);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, direction, Arrays.hashCode(vehicule));
+    }
 }
--- src/org/ies/vehicles/models/VehiculeType.java
+++ src/org/ies/vehicles/models/VehiculeType.java
@@ -1,4 +1,5 @@
 package org.ies.vehicles.models;
 
 public enum VehiculeType {
+
 }
